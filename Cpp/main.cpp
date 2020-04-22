e4ef6ebe7b42e7f9165c99c48191237e88265d04
Ashwani Kumar <53987093+ashwani8958@users.noreply.github.com>
2020-04-22 13:45:34 +0530

    Initial Commit

---
+++ Cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Cpp
+//
+//  Created by Ashwani on 22/04/20.
+//  Copyright © 2020 Ashwani. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
