89fc4faa77718fe18fd898852f09ae280aae3278
Ashwani Kumar <53987093+ashwani8958@users.noreply.github.com>
2020-04-25 13:08:35 +0530

    commit

---
+++ 5_OOP/1_Class/9_MovieLibrary/main.cpp
@@ -0,0 +1,50 @@
+//
+//  main.cpp
+//  5_OOP
+//
+//  Created by Ashwani on 25/04/20.
+//  Copyright © 2020 Ashwani. All rights reserved.
+//
+
+#include "Movies.hpp"
+
+void incrementWatched(Movies &movies, string name)
+{
+    if(movies.incrementWatched(name))
+        cout << name << " watch incremented" <<  endl;
+    else
+        cout << name << " not found" <<  endl;
+}
+
+void addMovie(Movies &movies, string name, string rating, int watched)
+{
+    if(movies.addMovies(name, rating, watched))
+        cout << name << " added" << endl;
+    else
+        cout << name << " already exists" <<  endl;
+}
+int main()
+{
+    Movies myMovies;
+    
+    myMovies.display();
+    
+    addMovie(myMovies, "Big", "PG-13",2);                           // OK
+    addMovie(myMovies,"Star Wars", "PG",5);                         // OK
+    addMovie(myMovies,"Cinderella", "PG",7);                        // OK
+    
+    myMovies.display();                                             // Big, Star Wars, Cinderella
+    
+    addMovie(myMovies,"Cinderella", "PG",7);                        // Already exists
+    addMovie(myMovies,"Ice Age", "PG",12);                          // OK
+    
+    myMovies.display();                                             // Big, Star Wars, Cinderella, Ice Age
+    
+    incrementWatched(myMovies,"Big");                               // OK
+    incrementWatched(myMovies,"Ice Age");                           // OK
+    
+    myMovies.display();                                             // Big and Ice Age watched count incremented by 1
+    
+    incrementWatched(myMovies,"XXX");                               // XXX not found
+    return 0;
+}
