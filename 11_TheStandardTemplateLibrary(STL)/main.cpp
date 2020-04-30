ace1b2621ca08fd0fdd8a5445e12aecb8a6aff33
Ashwani Kumar <53987093+ashwani8958@users.noreply.github.com>
2020-04-30 16:37:07 +0530

    commit

---
+++ 11_TheStandardTemplateLibrary(STL)/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  11_TheStandardTemplateLibrary(STL)
+//
+//  Created by Ashwani on 30/04/20.
+//  Copyright © 2020 Ashwani. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
