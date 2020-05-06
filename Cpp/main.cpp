b32e995ecbaa0272d575877aaaa4f5e557cf87f0
Ashwani Kumar <53987093+ashwani8958@users.noreply.github.com>
2020-05-06 13:40:51 +0530

    commit

--- Cpp/main.cpp
+++ Cpp/main.cpp
@@ -1,15 +1,51 @@
 //
 //  main.cpp
-//  Cpp
+//  1_FileOperation
 //
-//  Created by Ashwani on 22/04/20.
+//  Created by Ashwani on 30/04/20.
 //  Copyright © 2020 Ashwani. All rights reserved.
 //
 
-#include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <numeric>
+
+using namespace std;
+
+#if 0
+
+#endif
+
+#if 1
+int main()
+{
+    string wordToFind, readWordfromfile, filename = "../3_WordFinder/romeoandjuliet.txt", line;
+    int totalCountWord{0}, foundWordCount{0};
+    
+    ifstream inFile;
+    
+    inFile.open(filename);
+    if(!inFile)
+    {
+        cout << "Error in opening file: " << filename << endl;
+        return 1;
+    }
+    
+    cout << "Enter the Substring to search: " ;
+    cin >> wordToFind;
+    
+    while(inFile >> readWordfromfile)
+    {
+        totalCountWord++;
+        if(readWordfromfile.find(wordToFind) == std::string::npos)
+                foundWordCount++;
+            
+    }
+   
+    cout << totalCountWord << " word were searched\n";
+    cout << "The substring " << wordToFind << " was found " << foundWordCount << " times " << endl;
     return 0;
 }
+#endif
